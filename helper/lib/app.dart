af1fad08b0ea96636c57c39ed6ac14bf1a2da1ac
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-07-19 18:31:11 +0300

    19/07/2024

--- helper/lib/app.dart
+++ helper/lib/app.dart
@@ -1,5 +1,5 @@
 class App{
-  static const apiKey="AIzaSyA_u8y8SJNAyai76DWLDin5mWi3VPemOgY";
+  static const apiKey="";
   static const name="helper";
   static const appName="helper_flutter";
   static const appDirName="helper";
